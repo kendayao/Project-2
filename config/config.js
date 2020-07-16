d988c0ac3569b81903de2ea7b7c947722658a434
Kaeneth Dayao <kendayao@gmail.com>
2020-07-15 18:25:41 -0700

    heroku

--- config/config.js
+++ config/config.js
@@ -9,7 +9,7 @@ module.exports = {
     "dialect": "mysql"
   },
   "production": {
-    "use_env_variable": "mysql://h4fx1gplesq19t66:h4x6cu2iw429ry9r@x40p5pp7n9rowyv6.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/hc6w9i4t1g6a6rqq",
+    "use_env_variable": "JAWSDB_URL",
     "dialect": "mysql"
   }
 }
